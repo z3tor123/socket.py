3d6e270660fc7f09228b1f400003f8e686047c82
z3tor123 <72771279+z3tor123@users.noreply.github.com>
2021-08-27 05:11:58 +0200

    Add files via upload
    2 scan  hosts and ports

---
+++ socket.py
@@ -0,0 +1,26 @@
+import socket
+
+while True:
+    print("---------------------")
+    print("1) scan host")
+    print("2) scan name_port")
+    print("3) scan number_port")
+    print("---------------------")
+    x = int(input("what you wan't ( 1 - 2 -3 ) : "))
+    if x == 1:
+        hostname = input("Enter the hostname : ")
+        ip = socket.gethostbyname(hostname)
+        print("---------------------")
+        print( hostname + " ip = " + ip)
+
+    if x == 2:
+        port_name = input("Enter the port name : ")
+        port = socket.getservbyname(port_name)
+        print("----------------------")
+        print( port_name + " port number : " + str(port))
+
+    if x == 3:
+        port_number = int(input("Enter the port number : "))
+        service = socket.getservbyport(port_number)
+        print("-----------------------")
+        print( str(port_number) + " port name : " + str(service))
