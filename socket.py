d339a375f6727944a0269b330f4ae453ec78bc5d
z3tor123 <72771279+z3tor123@users.noreply.github.com>
2021-08-29 00:42:23 +0200

    Update socket.py

--- socket.py
+++ socket.py
@@ -1,5 +1,5 @@
 import socket
-
+import time
 
 print("---------------------")
 print("1) scan host")
@@ -12,19 +12,32 @@ if x == 1:
     ip = socket.gethostbyname(hostname)
     print("---------------------")
     print( hostname + " ip = " + str(ip))
-    exit()
+    q2 = input("want 2 exit ( y - n )")
+    if q2 == "y" or "yes" or "Y" or "Yes":
+        exit()
+    elif q2 == "n" or "no" or "N" or "No":
+        time.sleep(9999999999999999)
 
 if x == 2:
     port_name = input("Enter the port name : ")
     port = socket.getservbyname(port_name)
     print("----------------------")
     print( port_name + " port number : " + str(port))
-    exit()
+    q1 = input("want 2 exit ( y - n )")
+    if q1 == "y" or "yes":
+        exit()
+    elif q1 == "n":
+        time.sleep(9999999999999999)
+    exit() 
 if x == 3:
     port_number = int(input("Enter the port number : "))
     service = socket.getservbyport(port_number)
     print("-----------------------")
     print( str(port_number) + " port name : " + service)
-    exit()
+    q3 = input("want 2 exit ( y - n )")
+    if q3 == "y" or "yes":
+        exit()
+    elif q3 == "n":
+        time.sleep(9999999999999999)
 else:
     print("(:")
